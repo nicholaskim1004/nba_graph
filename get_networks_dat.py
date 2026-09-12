98c66490f780d790aec29d1b9bebc16b571d2896
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-09-11 21:41:03 -0400

    creating the table in the database
    adding print statements since its getting error

--- get_networks_dat.py
+++ get_networks_dat.py
@@ -31,6 +31,8 @@ passes_yr = pd.read_sql_query(query, con)
 
 team_list = teams.get_teams()
 team_list = team_list[0]
+print(team_list)
+print(team_list[0]['full_name'])
 
 diff_shots = ['Restricted Area', 'In The Paint (Non-RA)', 'Mid-Range', 'Left Corner 3', 'Right Corner 3', 'Above the Break 3', 'Backcourt']
 
