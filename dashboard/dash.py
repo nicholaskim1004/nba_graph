91bf83a2ec8ccdb44518bc0b006e41691c7cef8a
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-09-16 14:49:39 -0400

    adding div and header components to test visual

--- dashboard/dash.py
+++ dashboard/dash.py
@@ -11,3 +11,12 @@ pageranks_yr = pd.read_sql_query(query_sh, con)
 
 app = Dash()
 
+app.layout = html.Div(childern = [
+    html.H1("NBA GRAPH NETWORKS"),
+    html.Div(["How are NBA Offense's different? Do teams follow a similar pattern? Do strategies change in the Playoffs?",
+              
+              "All these questions can be answered using network structure! This dashboard will display the results from a network for a given season, and team."])
+])
+
+if __name__ == "main":
+    app.run(debug=True)
