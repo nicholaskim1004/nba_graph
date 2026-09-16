8a94c70453a3e660bf6fc019706d02d2eefeb533
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-09-16 14:54:29 -0400

    have to specify children

--- dashboard/dash_script.py
+++ dashboard/dash_script.py
@@ -12,8 +12,8 @@ pageranks_yr = pd.read_sql_query(query_sh, con)
 app = Dash()
 
 app.layout = html.Div(childern = [
-    html.H1("NBA GRAPH NETWORKS"),
-    html.Div(["How are NBA Offense's different? Do teams follow a similar pattern? Do strategies change in the Playoffs?",
+    html.H1(children = "NBA GRAPH NETWORKS"),
+    html.Div(children = ["How are NBA Offense's different? Do teams follow a similar pattern? Do strategies change in the Playoffs?",
               
               "All these questions can be answered using network structure! This dashboard will display the results from a network for a given season, and team."])
 ])
