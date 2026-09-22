5adfe5b6efb860b6d2e9b0d8a85009ba932aa6f5
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-09-21 22:08:16 -0400

    added import dash and changed tab to link

--- dashboard/homepage.py
+++ dashboard/homepage.py
@@ -1,11 +1,12 @@
 # app.py
+import dash
 from dash import Dash, html, dcc
 app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
     html.H1("NBA GRAPH NETWORKS"),
-    dcc.Tabs(id='tabs', value=None, children=[
-        dcc.Tab(label=page['name'], value=page['relative_path'])
+    html.Div([
+        dcc.Link(page['name'], href=page['relative_path'])
         for page in dash.page_registry.values()
     ]),
     dash.page_container  # Dash swaps content here based on URL
