4f5633d589b958b3400b108f518d0564e142780f
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-09-24 22:20:18 -0400

    need to get the positions for each player to create a model to find underratted players. script to get this data

---
+++ data/get_position_dat.py
@@ -0,0 +1,54 @@
+import time
+import sqlite3
+import pandas as pd
+
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import CommonPlayerInfo
+
+con = sqlite3.connect('data/nba.db', timeout=10)
+cursor = con.cursor()
+
+#drop pagerank table if it exists to avoid duplicates
+cursor.execute("DROP TABLE IF EXISTS positions")
+
+#set up table for storing position info for each player
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS positions
+               (
+                   player_id INTEGER,
+                   player_name TEXT,
+                   positions TEXT
+               )
+               """)
+
+query = "SELECT DISTINCT player_id, player_name FROM shots_yr"
+unique_players = pd.read_sql_query(query, con)
+
+seen = {}
+
+N = len(unique_players)
+
+print('starting data pull')
+
+for i, row in unique_players.iterrows():
+
+    print(f'progress: {(i+1)/N * 100}')
+    try:
+        playerid = row['player_id']
+        
+        if playerid not in seen.keys():
+            seen[playerid] = True
+            player_inf = CommonPlayerInfo(player_id=playerid).get_data_frames()[0]
+            
+            pd.DataFrame({'player_id': playerid, 'player_name': row['player_name'], 'POSITION': player_inf['POSITION']}, index=[0]).to_sql('positions', con, if_exists='append', index=False)
+        else: 
+            #skip
+            continue
+            
+    except Exception as e:
+        print(f'having issues with {row['player_name']}: {e}') 
+    
+    #avoid rate limit
+    time.sleep(1)
+    
+print('finished')
