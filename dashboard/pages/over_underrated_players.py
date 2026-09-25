50c08e74e51fa29ca56ad8d06733d3d8d34f173e
nicholaskim1004 <nicholaskim.1010@gmail.com>
2026-09-24 23:57:55 -0400

    correctly pulled in pos

--- dashboard/pages/over_underrated_players.py
+++ dashboard/pages/over_underrated_players.py
@@ -12,17 +12,11 @@ pos = pd.read_sql_query(query_pos, con)
 query_page = 'SELECT * FROM pageranks_yr'
 pageranks = pd.read_sql_query(query_page, con)
 
-query_id = 'SELECT DISTINCT player_id, player_name FROM shots_yr'
-player_ids = pd.read_sql_query(query_id, con)
-
 #standarizing naming convention for some 
 pos.loc[pos['positions']=='Forward-Guard','positions'] = 'Guard-Forward'
 pos.loc[pos['positions']=='Forward-Center','positions'] = 'Center-Forward'
 
-#mergining on player id to pagerank
-#pageranks = pd.merge(pageranks.loc[:,['season','team_id','node_name','pagerank']], player_ids, left_on='node_name', right_on='player_name', how='left' )
-print(pageranks.head())
-print(player_ids)
+
 #merging in pos to pagerank
-#pos = pd.merge(pageranks,pos.loc[:,['player_name','positions']], left_on='node_name', right_on='player_name', how='left')
-#print(pos.head())
+pos = pd.merge(pageranks.loc[:,['season','team_id','node_name','player_id']],pos.loc[:,['player_id','positions']], on='player_id', how='left')
+print(pos.head())
